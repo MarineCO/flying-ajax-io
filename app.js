4e43c9e87d9c6fe0e32f03e4c85faad4920e9672
marineco <marinecolonge@gmail.com>
2016-10-24 15:50:57 +0200

    Ajout de l'affichage des places, affichage du mot de passe en cours

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 $('#show').click(function(){
 	$.ajax('http://192.168.1.21:3000/places')
 	.done(ajaxDone)
-	.fail(ajaxFail);
+	.fail(ajaxFail)
+	.always(ajaxAlways);
 
 	function ajaxDone(response) {
-		console.log(response);
-	}
+	    var listplaces = response["places"];
+	    console.log(listplaces);
+	    //Affichage des places sur la page web -> marche
+	    for (var i = 0; i < listplaces.length; i++) {
+	      	$('ul').append('<li>' + listplaces[i]["nom"] + '</li>');
+	    }
+		
+		//Affichage du mot de passe sur le body -> ne marche pas
+		if(listplaces === listplaces["IoT Valley"]){
+         	var motDePasse = listplaces["IOT Valley"]["password"];
+         	$('body').text("mot de passe:" + motDePasse);
+      	}      
+   	}
 
 	function ajaxFail(response) {
-		console.log(response);
+		console.log("Fail");
 	}
 
+	function ajaxAlways(completer){
+		console.log("Always");
+	}
 });
